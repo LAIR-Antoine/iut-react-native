11c44bc48870a28240e4998a83daff55ceb85fe9
LAIR-Antoine <lairantoine@orange.fr>
2023-12-15 09:54:25 +0100

    feat : display one pokemon

--- App.js
+++ App.js
@@ -1,10 +1,48 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
+  const [pokemon, setPokemon] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const url = 'https://pokeapi.co/api/v2/pokedex/1/';
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setPokemon(data.pokemon_entries);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <Text>Loading...</Text>;
+  }
+
+  if (error) {
+    return <Text>Error: {error.message}</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {pokemon && pokemon.length > 0 && (
+        <>
+          <Image
+            style={{ width: 200, height: 200 }}
+            source={{
+              uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + pokemon[0].entry_number + '.png',
+            }}
+          />
+          <Text>{pokemon[0].pokemon_species.name}</Text>
+        </>
+      )}
       <StatusBar style="auto" />
     </View>
   );
@@ -18,3 +56,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
